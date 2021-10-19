478b7443f5ff314c87418ce8f3095a3ac35ad4af
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-10-19 17:16:44 +0300

    ....

--- scooters/src/components/NewScooter.jsx
+++ scooters/src/components/NewScooter.jsx
@@ -41,14 +41,17 @@ function NewScooter({ addScooter }) {
                 <div className="form-row" style={{ marginTop: "50px" }}>
                     <div className="col-sm-3 col-form-label">
                         <input type="text" maxLength="8" onKeyPress={(event) => { if (!/[0-9]/.test(event.key)) { event.preventDefault(); } }} onChange={(e) => control(e, "registration_code")} value={code} className="form-control" id="validationDefault01" placeholder="Registracijos numeris" required />
+                        <small>Registracijos numeris</small>
                     </div>
                     <div className="col-sm-3 col-form-label">
                         <input type="date" onChange={(e) => control(e, "last_use_time")} value={useTime} className="form-control" id="validationDefault02" placeholder="Pakutinis naudojimo laikas" required />
+                        <small>Data</small>
                     </div>
                     <div className="col-sm-3 col-form-label">
                         <input type="number" onChange={(e) => control(e, "total_ride_kilometres")} value={ride} className="form-control" id="validationDefault02" placeholder="Pravažiuoti kilometrai" required />
+                        <small>Kilometrai</small>
                     </div>
-                    <button className="btn btn-primary m-1" type="submit" style={{ textAlign: "center"}} onClick={insert}>Įvesti naują paspirtuką</button>
+                    <button className="btn btn-primary m-1" type="submit" style={{ textAlign: "center", height: "45px" }} onClick={insert}>Įvesti naują paspirtuką</button>
                 </div>
             </form>
         </>
