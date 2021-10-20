d0eeb06aab33dc3d6032ddd4184d475dc134c4df
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-10-20 13:40:23 +0300

    ...

--- scooters/src/components/NewScooter.jsx
+++ scooters/src/components/NewScooter.jsx
@@ -37,19 +37,19 @@ function NewScooter({ addScooter }) {
     };
 
     return (
-        <div className="newScooter col-12 col-lg-12 ml-lg-12 col-sm-12 col-md-12 col-xl-12">
-            <form className="form-group new">
-                <h4 style={{ color: "lightgrey" }}>Įvesti naują paspirtuką</h4>
-                <div className="form-row">
-                    <div className=" col-form-label">
+        <div>
+            <form>
+                <h4 style={{ color: "lightgrey", marginTop: "50px" }}>Įvesti naują paspirtuką</h4>
+                <div className="form-row newScooter">
+                    <div className=" col-form-label col-auto col-sm-auto col-md-auto">
                         <input type="text" maxLength="8" onKeyPress={(event) => { if (!/[0-9]/.test(event.key)) { event.preventDefault(); } }} onChange={(e) => control(e, "registration_code")} value={code} className="form-control" id="validationDefault01" placeholder="Registracijos numeris" required />
                         <small>Registracijos numeris</small>
                     </div>
-                    <div className=" col-form-label">
+                    <div className=" col-form-label col-auto col-sm-auto col-md-auto">
                         <input type="date" onChange={(e) => control(e, "last_use_time")} value={useTime} className="form-control" id="validationDefault02" placeholder="Pakutinis naudojimo laikas" required />
                         <small>Data</small>
                     </div>
-                    <div className=" col-form-label">
+                    <div className=" col-form-label col-auto col-sm-auto col-md-auto">
                         <input type="number" onChange={(e) => control(e, "total_ride_kilometres")} value={ride} className="form-control" id="validationDefault02" placeholder="Pravažiuoti kilometrai" required />
                         <small>Kilometrai</small>
                     </div>
