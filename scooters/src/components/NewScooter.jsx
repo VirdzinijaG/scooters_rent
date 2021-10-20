6dc8bceebafeae00b607a4ebda455802aa761874
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-10-20 14:43:26 +0300

    ...

--- scooters/src/components/NewScooter.jsx
+++ scooters/src/components/NewScooter.jsx
@@ -42,16 +42,16 @@ function NewScooter({ addScooter }) {
                 <h4 style={{ color: "lightgrey", marginTop: "50px" }}>Įvesti naują paspirtuką</h4>
                 <div className="form-row newScooter">
                     <div className=" col-form-label col-auto col-sm-auto col-md-auto">
-                        <input type="text" maxLength="8" onKeyPress={(event) => { if (!/[0-9]/.test(event.key)) { event.preventDefault(); } }} onChange={(e) => control(e, "registration_code")} value={code} className="form-control" id="validationDefault01" placeholder="Registracijos numeris" required />
-                        <small>Registracijos numeris</small>
+                        <input type="text" maxLength="8" onKeyPress={(event) => { if (!/[0-9]/.test(event.key)) { event.preventDefault(); } }} onChange={(e) => control(e, "registration_code")} value={code} className="form-control" id="validationDefault01" placeholder="12345678" required />
+                        <small style={{color: "white"}}>Registracijos numeris</small>
                     </div>
                     <div className=" col-form-label col-auto col-sm-auto col-md-auto">
-                        <input type="date" onChange={(e) => control(e, "last_use_time")} value={useTime} className="form-control" id="validationDefault02" placeholder="Pakutinis naudojimo laikas" required />
-                        <small>Data</small>
+                        <input type="date" onChange={(e) => control(e, "last_use_time")} value={useTime} className="form-control" id="validationDefault02" required />
+                        <small style={{color: "white"}}>Data</small>
                     </div>
                     <div className=" col-form-label col-auto col-sm-auto col-md-auto">
-                        <input type="number" onChange={(e) => control(e, "total_ride_kilometres")} value={ride} className="form-control" id="validationDefault02" placeholder="Pravažiuoti kilometrai" required />
-                        <small>Kilometrai</small>
+                        <input type="number" onChange={(e) => control(e, "total_ride_kilometres")} value={ride} className="form-control" id="validationDefault02" placeholder="100" required />
+                        <small style={{color: "white"}}>Kilometrai</small>
                     </div>
                     <div className="insertBtn">
                         <button className="btn btn-primary m-1" type="submit" onClick={insert}>Įvesti naują paspirtuką</button>
